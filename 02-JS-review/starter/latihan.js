7a87d9a87b49ce93398d90dc2cfecf6968c73ac9
Muhammad Raihan <m.rhn.ph@gmail.com>
2025-07-31 22:35:20 +0700

    latihan 2

--- 02-JS-review/starter/latihan.js
+++ 02-JS-review/starter/latihan.js
@@ -49,3 +49,55 @@ const book = {
 
 const judul = book.title, penulis = book.author, genreUtama = book.genres[0], tahunPublikasi = book.publication.year;
 console.log("Judul: " + judul + ", Genre: " + genreUtama + ", Tahun: " + tahunPublikasi)
+
+
+// 1.
+const profile = {
+  username: "raihan_putra",
+  email: "raihan@example.com",
+  verified: true
+};
+
+// TODO:
+// 1. Ambil username tapi simpan ke variabel dengan nama 'namaPengguna'
+// 2. Cetak ke konsol: "Username: raihan_putra (verified: true)"
+
+const {username : namaPengguna, verified} = profile; //username : namaPengguna is for storing username to namaPengguna
+console.log("Username: " + namaPengguna + " (verified: " + verified);
+
+//------------------------------------------------------------------------------------------------------ 
+
+// 2. 
+const hasil = ["Sukses"];
+
+// TODO:
+// 1. Ambil elemen pertama sebagai 'status'
+// 2. Ambil elemen kedua sebagai 'kode', jika tidak ada beri default "000"
+// 3. Cetak ke konsol: "Status: Sukses, Kode: 000"
+
+const [status, kode = "000"] = hasil; //kode = "000" is default parameter which means if there is no value, it will print the default parameter instead of undefined
+console.log("Status : " + status + ", " + "Kode: " + kode)
+
+// -------------------------------------------------------------------------------------------------------------
+
+// 3.
+const response = {
+  data: {
+    user: {
+      id: 42,
+      name: "Raihan"
+    },
+    token: "abc123"
+  }
+};
+
+// TODO:
+// 1. Buat fungsi `tampilkanUser` yang menerima parameter berupa objek di atas
+// 2. Gunakan destructuring langsung di parameter fungsi
+// 3. Cetak: "ID: 42, Nama: Raihan, Token: abc123"
+
+function tampilkanUser({data: {user: {id, name}, token}}) {
+    console.log("ID: " + id + ", " + "Nama: " + name + " Token: " + token);
+}
+
+tampilkanUser(response);
